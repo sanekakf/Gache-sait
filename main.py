f90fd355adcf2f376028935dc2ce97bf59535d1c
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2021-08-07 23:48:57 +0300

    gachi sait

---
+++ main.py
@@ -0,0 +1,13 @@
+#Основа для сайта, серверная часть
+
+from os import name
+from flask import Flask, render_template, redirect
+
+app= Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
