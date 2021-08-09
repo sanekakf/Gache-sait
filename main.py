0ac4e6ba33e8d290441f5f1f952fd37457fc7d52
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2021-08-09 23:58:41 +0300

    aboba

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-#Основа для сайта, серверная часть
+# Основа для сайта, серверная часть
 
 from os import name
 from flask import Flask, render_template, redirect
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
