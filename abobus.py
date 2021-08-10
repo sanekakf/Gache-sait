edd01e86fd6c884325f9deb21a632d7cfa6234d3
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2021-08-10 21:49:59 +0300

    Uploading site from aboba

--- abobus.py
+++ abobus.py
@@ -1,9 +1,20 @@
 # Основа для сайта, серверная часть
 
-from os import name
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect
+import psycopg2 as ps
+import psycopg2.extras
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'aboba'
+
+
+DB_HOST = 'ec2-54-170-163-224.eu-west-1.compute.amazonaws.com'
+DB_NAME = 'df043ppajn3au9'
+DB_USER = 'lfcjpjxpmcfqxi'
+DB_PASS = '70ec9d90f402e4102fb1ea1d8699a2a0c232034016d6edacd6d681093a20772b'
+conn = ps.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 
 @app.route('/')
@@ -21,18 +32,42 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST','GET'])
 def login():
+    if request.method == 'POST':
+        if request.method == 'POST':
+            cur.execute('SELECT * FROM users')
+            print(cur.fetchall())
+            name = request.form.get('login')
+            password = request.form.get('password')
+            try:
+                cur.execute('SELECT * FROM users WHERE login=' + name)
+                user = cur.fetchall()
+                cur.execute('SELECT password FROM users WHERE login = %s', (name,))
+                _pass=cur.fetchall()
+                print(_pass)
+                if _pass[0][0] == password:
+                    flash('Вход был выполнен успешно', category='success')
+                    return redirect('/pricing')
+
+                else:
+                    flash('Пароль или логин не совпадает', category='error')
+            except Exception:
+                flash('Такого аккаунта не существует, создайте его по ссылке ниже', category='error')
     return render_template('login.html')
 
 
-@app.route('/register', methods=['POST'])
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        login=request.form.get('login')
-        password=request.form.get('password')
-        datas={'login':login , 'password':password}
-        print(datas)
+        name = request.form.get('login')
+        password = request.form.get('password')
+        cur.execute('INSERT INTO users VALUES (%s,%s)', (str(name), str(password)))
+        conn.commit()
+        cur.execute('SELECT * FROM users')
+        print(cur.fetchall())
+        flash('Аккаунт успешно создан', category='success')
+        return redirect('/login')
     return render_template('register.html')
 
 
@@ -42,4 +77,7 @@ def update():
 
 
 if __name__ == '__main__':
+    cur.execute('DROP TABLE users')
+    cur.execute('CREATE TABLE IF NOT EXISTS users (login TEXT PRIMARY KEY, password TEXT)')
+    conn.commit()
     app.run(debug=True)
