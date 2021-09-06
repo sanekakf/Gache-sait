209e1513a24112dbc23e70783e6b9a0cbb9d6499
Sanekakf <83131846+sanekakf@users.noreply.github.com>
2021-09-06 20:36:13 +0300

    Глобальный update

--- app.py
+++ app.py
@@ -265,4 +265,4 @@ if __name__ == '__main__':
             return None
     login_manager.init_app(app)
 
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5000)
